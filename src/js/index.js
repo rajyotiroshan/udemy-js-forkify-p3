10e2e1299252bb6bc47ad7c3703736e6d2acfff2
rajan <rajangreedge@gmail.com>
2019-03-30 14:29:02 +0530

    getResult in Search.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,8 @@
 /* import * as searchView from './views/searchView';
 console.log(`${string} ${searchView.add(searchView.ID,2)} ${searchView.ID}*2 = ${searchView.multiply(searchView.ID,2)}`); */
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const KEY = 'b196088c37e3c77a634148c530bbbc77';
-    const baseURL = 'https://www.food2fork.com/api/search';
-    try {
-            const res = await axios(`${baseURL}?key=${KEY}&q=${query}`);
-            const recipes = res.data.recipes;
-            console.log(recipes);  
-    }catch(error){
-        alert(error);
-    }
+const search= new Search('pizza');
 
-}
-
-getResults('chicken curry');
+console.log(search.getResults());
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
 /* export default 'an string default export'; */
+import axios from "axios";
+
+export default class Search {
+
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const KEY = "b196088c37e3c77a634148c530bbbc77";
+    const baseURL = "https://www.food2fork.com/api/search";
+    try {
+      const res = await axios(`${baseURL}?key=${KEY}&q=${this.query}`);
+      this.result = res.data.recipes;
+     // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
