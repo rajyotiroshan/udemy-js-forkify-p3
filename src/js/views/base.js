2b9ac1ac8a62464af445f14880858aa7d193b52c
rajan <rajangreedge@gmail.com>
2019-04-12 18:15:56 +0530

    access recipe

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,7 +3,8 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchResultList: document.querySelector('.results__list'),
     searchRes: document.querySelector('.results__list'), 
-    searchResPages: document.querySelector('.results__pages')
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe')
 };
 
 export const elementString = {
